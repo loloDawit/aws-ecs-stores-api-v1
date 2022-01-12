8ffc57df0e400d3295a0f7ebe5c1da1353864ef4
dawit.abera <dawit.y.abera@nordstrom.com>
2022-01-12 12:14:47 -0800

    fixing error

--- models/Users.js
+++ models/Users.js
@@ -7,7 +7,7 @@ const UserSchema = new mongoose.Schema({
   name: {
     type: String,
     required: [true, 'Please add a name'],
-    maxlength: [15, 'Name can not be more than 50 characters'],
+    maxlength: [15, 'Name can not be more than 50 characters']
   },
   email: {
     type: String,
@@ -15,26 +15,26 @@ const UserSchema = new mongoose.Schema({
     unique: true,
     match: [
       /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-      'Please use a valid email',
-    ],
+      'Please use a valid email'
+    ]
   },
   role: {
     type: String,
     enum: ['user', 'datapublisher'],
-    default: 'user',
+    default: 'user'
   },
   password: {
     type: String,
     required: [true, 'Please add a password'],
     minlength: 8,
-    select: false,
+    select: false
   },
   resetPasswordToken: String,
   resetPasswordExpire: Date,
   createdAt: {
     type: Date,
-    default: Date.now,
-  },
+    default: Date.now
+  }
 });
 
 /**
@@ -52,8 +52,9 @@ UserSchema.pre('save', async function (next) {
  *
  */
 UserSchema.methods.getSignedJSONWebToken = function () {
+  console.log('from model', process.env.JWT_SECRET);
   return JsonWebToken.sign({ id: this._id }, process.env.JWT_SECRET, {
-    expiresIn: process.env.JWT_EXPIRE_DATE,
+    expiresIn: process.env.JWT_EXPIRE_DATE
   });
 };
 /**
@@ -68,10 +69,7 @@ UserSchema.methods.validateHashedPassword = async function (password) {
 UserSchema.methods.getPasswordResetToken = function () {
   const resetToken = crypto.randomBytes(20).toString('hex'); // generate token
   // hash it
-  this.resetPasswordToken = crypto
-    .createHash('sha256')
-    .update(resetToken)
-    .digest('hex');
+  this.resetPasswordToken = crypto.createHash('sha256').update(resetToken).digest('hex');
   // set expire
   this.resetPasswordExpire = Date.now() + 5 * 60 * 1000;
   return resetToken;
